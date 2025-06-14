050d7099422282399ed86fd0d2652ce348b3cdbe
nffctaku <footballtopjp96@gmail.com>
2025-06-15 03:18:49 +0900

    feat: add MatchInfo type definition

---
+++ types/match.ts
@@ -0,0 +1,13 @@
+// types/match.ts
+
+export type MatchInfo = {
+  teamA: string;
+  teamB: string;
+  competition: string;
+  season: string;
+  nickname: string;
+  stadium?: string;
+  seat?: string;
+  seatReview?: string;
+  ticketPrice?: number;
+};
