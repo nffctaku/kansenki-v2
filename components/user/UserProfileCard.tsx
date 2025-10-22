f679456c6ee2e4a2018c699413a9b4f043b68093
nffctaku <footballtopjp96@gmail.com>
2025-10-22 10:01:58 +0900

    fix: UserProfileCardから不要なresidenceプロパティを削除

--- components/user/UserProfileCard.tsx
+++ components/user/UserProfileCard.tsx
@@ -21,7 +21,6 @@ export const UserProfileCard = (props: UserProfileProps) => {
     travelFrequency, setTravelFrequency,
     overseasMatchCount, setOverseasMatchCount,
     visitedCountries, setVisitedCountries,
-    residence, setResidence,
     xLink, setXLink,
     instagramLink, setInstagramLink,
     youtubeLink, setYoutubeLink,
