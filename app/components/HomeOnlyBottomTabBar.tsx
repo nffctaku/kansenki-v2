e9214520d004727a2b7b886cdf7c917f26021889
nffctaku <footballtopjp96@gmail.com>
2026-04-01 10:06:14 +0900

    🚀 本番環境に向けて調整・デプロイ準備

--- app/components/HomeOnlyBottomTabBar.tsx
+++ app/components/HomeOnlyBottomTabBar.tsx
@@ -6,7 +6,7 @@ import BottomTabBar from './BottomTabBar';
 export default function HomeOnlyBottomTabBar() {
   const pathname = usePathname();
 
-  if (pathname !== '/home') return null;
+  if (pathname !== '/home' && pathname !== '/timeline') return null;
 
   return <BottomTabBar />;
 }
